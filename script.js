d2c25c4312cfb071281df1b9b77129c674fe3571
Andrew Thomson <athomson1711@gmail.com>
2022-05-28 10:04:50 +0100

    removed newGame btn func due to reload bug

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-const newGameBtn = document.querySelector(".new-game-button");
+const newGameBtn = document.querySelector(".new-game-btn");
 // const playArea = document.querySelector(".play-area");
 const cardContainer = document.querySelector(".card-container")
 
-const cardNumbers = [1,2,3,4,5,6,7,8,9,10]
+const cardNumbers = [1,1,2,2,3,3,4,4,5,5]
 
-cardNumbers.map(number => {
-    const card = document.createElement("p");
-    card.innerHTML = number;
-    card.setAttribute("class", "card");
-    cardContainer.appendChild(card);  
+// Function to start new game and layout cards
+// NEED TO add a way to randomly generate cards
+
+// newGameBtn.addEventListener("click", (event) => {
+        const dealCards = cardNumbers.map(number => {
+        const playCard = document.createElement("p");
+        playCard.innerHTML = number;
+        playCard.setAttribute("class", "card");
+        cardContainer.appendChild(playCard);  
+    })
+// })
+
+// CHECKS "NEW GAME" BUTTON WORKS
+newGameBtn.addEventListener("click", (event) => {
+   console.log("New Game Button Clicked")
 })
+
+
+const cards = document.querySelectorAll("p")
+
+cards.forEach(card => {
+    card.addEventListener("click", (event) =>{
+        console.log(`Card value is ${card.innerHTML}`)
+    })
+})
+
+
 
